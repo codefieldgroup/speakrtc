9af4d9c88939c6538ffdd6759376d0a9c3867195
Victor Cruz <vitycruz@gmail.com>
2014-06-21 07:00:10 -0400

    EasyRTC ADD Include easyrtc and configure in server.

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,16 +1,25 @@
-var io = require('socket.io');
+var io = require('socket.io'),
+  easyrtc = require("easyrtc");
 
 exports.socketServer = function (app, server) {
   var socket = io.listen(server);
   socket.set('log level', 1);
-  socket.set("transports", ["xhr-polling"]);
-  socket.set('browser client minification', true);
+  //socket.set("transports", ["xhr-polling"]);
+  //socket.set('browser client minification', true);
   //socket.set('browser client gzip', true);
 
   module.exports.export = socket;
 
+  /**
+   * Socket operations.
+   */
   socket.sockets.on('connection', function (client) {
     console.log("New client is here!");
 
   });
+
+  /**
+   * EasyRTC operations.
+   */
+  var rtc = easyrtc.listen(app, socket);
 };
